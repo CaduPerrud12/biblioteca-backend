965e040f1dbdb47082ddff9634f94b7814ac24b8
CaduPerrudGerminare <carlos.sousa@germinare.org.br>
2026-04-10 09:52:29 -0300

    commit inicial

---
+++ src/main/java/org/example/biblioteca/bibliotecaatlas/BibliotecaAtlasApplication.java
@@ -0,0 +1,13 @@
+package org.example.biblioteca.bibliotecaatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAtlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaAtlasApplication.class, args);
+    }
+
+}
